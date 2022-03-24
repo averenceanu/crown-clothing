72ab052dba6de933b3500582de46e08b9d5201c9
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-03-24 14:37:34 -0400

    Add SASS to project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-
+import './categories.styles.scss'
 
 function App() {
+  
+  const categories = [
+    { 
+      id: 1,
+      title: 'Hats', 
+     },
+     { 
+      id: 1,
+      title: 'Jackets', 
+     },
+     { 
+      id: 1,
+      title: 'Sneakers', 
+     },
+     { 
+      id: 1,
+      title: 'Womens', 
+     },
+     { 
+      id: 1,
+      title: 'Mens', 
+     },
+  ]
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img />  */}
-        <div className="category-body-container"> 
-          <h2> Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map(({title}) => {  
+        return (
+          <div className="category-container">
+            <div className="background-image"/>
+            <div className="categor y-body-container"> 
+              <h2> {title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
