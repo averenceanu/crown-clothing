32fbf98798c5b15e97953cd0fd9227d70a2e6ece
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-03-25 15:12:26 -0400

    Add event.target to input fields Signup form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,19 +1,58 @@
+import { useState } from 'react';
+
+const defaultFormFiels = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
+
 const SignUpForm = () => {
+  const [formFiels, setFormFiels] = useState(defaultFormFiels)
+  const {displayName, email, password, confirmPassword} = formFiels;
+
+  console.log(formFiels)
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setFormFiels({...formFiels, [name]: value});
+  }
+ 
   return (
     <div>
       <h1> Sign Up with your Email and Password</h1>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" required/>
+        <input 
+          type="text" 
+          name="displayName" 
+          onChange={handleChange} 
+          value={displayName}
+          required/>
 
         <label>Email</label>
-        <input type="email" required/>
+        <input 
+          type="email" 
+          name="email" 
+          onChange={handleChange} 
+          value={email}
+          required/>
 
         <label>Password</label>
-        <input type="password" required/>
+        <input 
+          type="password" 
+          name="password" 
+          onChange={handleChange} 
+          value={password}
+          required/>
 
         <label>Confirm Password</label>
-        <input type="password" required/>
+        <input 
+          type="password" 
+          name="confirmPassword" 
+          onChange={handleChange} 
+          value={confirmPassword}
+          required/>
+
         <button type="submit">Sign Up</button>
       </form>
     </div>
