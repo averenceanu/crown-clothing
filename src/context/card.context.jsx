1e038aee3abc465ca9fe3858328703c748db542f
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-03-31 17:51:35 -0400

    style checkout item components

--- src/context/card.context.jsx
+++ src/context/card.context.jsx
@@ -30,12 +30,15 @@ const removeCartItem = (cartItems, cartItemToRemove) =>{
   )
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id); 
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [], 
   addItemToCart: () => {},
   removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
 })
 
@@ -57,7 +60,11 @@ export const CartProvider = ({children}) => {
     setCartItems(removeCartItem(cartItems, cartItemToRemove))
   }
 
-  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart};
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear))
+  }
+
+  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart, clearItemFromCart};
 
   return <CartContext.Provider value={value}> {children} </CartContext.Provider>
 }
