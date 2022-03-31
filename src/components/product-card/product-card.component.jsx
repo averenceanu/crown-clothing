91f7958f4902f97c02313a5c59879caf2b14ba63
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-03-31 15:16:50 -0400

    Add style to cart-item

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -5,14 +5,14 @@ import Button from '../button/button.component';
 import { CartContext } from '../../context/card.context';
 
 const ProductCard = ({product}) => {
-  const { name, price, imageURL } = product;
+  const { name, price, imageUrl } = product;
   const { addItemToCart } =useContext(CartContext);
 
   const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className='product-card-container'>
-      <img src={imageURL} alt={`${name}`}/>
+      <img src={imageUrl} alt={`${name}`}/>
       <div className='footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
