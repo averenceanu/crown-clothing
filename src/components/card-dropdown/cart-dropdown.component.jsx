d9f38a9738dcda30e0b735c4789f5b6be2b8834e
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-03-31 13:59:00 -0400

    Implement addCartItem function to add new item to card or change quantity when already existing

--- src/components/card-dropdown/cart-dropdown.component.jsx
+++ src/components/card-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,21 @@
+import { useContext } from 'react';
 import './card-dropdown.styles.scss';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { CartContext } from '../../context/card.context';
 
 const CartDropDown = () => {
+
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-
+        {cartItems.map(item => <CartItem cartItem={item} key={item.id}/>)}
       </div>
       <Button buttonType='inverted'>GO TO CHECKOUT</Button>
     </div>
   )
 }
 
-export default CartDropDown;
+export default CartDropDown; 
