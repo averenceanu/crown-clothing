ff1a5e54b9ec10297776c5714e100546a6bf74d4
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-04-01 00:38:08 -0400

    Setup shop/* route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
-
-
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home/>} />
-        <Route path='shop' element={<Shop />} />
+        <Route path='shop/*' element={<Shop />} />
         <Route path='sign-in' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
