6b674ad1cc02c23ca20f695cd1502a87460f70e4
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-04-11 14:13:02 -0400

    Refractor to add the bussines logic of categories in the selector file

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,1 +1,9 @@
-export const selectCategoriesMap = (state) => state.categories.categoriesMap;
+export const selectCategoriesMap = (state) =>
+  state.categories.categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+//in this file we are transforming the data that is passed down afterwards.
+//the business login is found in the selector file.
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,27 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { 
-  getAuth, 
-  //signInWithRedirect, 
-  signInWithPopup, 
+import {
+  getAuth,
+  //signInWithRedirect,
+  signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
-} from 'firebase/auth'
-import { 
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
   getFirestore,
-  doc, 
+  doc,
   getDoc,
   setDoc,
-  collection, 
+  collection,
   writeBatch,
   query,
-  getDocs
-} from 'firebase/firestore'
-// NOTES: getDoc and setDoc => getting the doc data and setting the doc data  
-
+  getDocs,
+} from "firebase/firestore";
+// NOTES: getDoc and setDoc => getting the doc data and setting the doc data
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -30,99 +29,98 @@ const firebaseConfig = {
   projectId: "crwn-db-cabc5",
   storageBucket: "crwn-db-cabc5.appspot.com",
   messagingSenderId: "792969065176",
-  appId: "1:792969065176:web:d8ba509e4f893a95b21dad"
+  appId: "1:792969065176:web:d8ba509e4f893a95b21dad",
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig); 
+const firebaseApp = initializeApp(firebaseConfig);
 
-//providers are Google, facebook and so on 
+//providers are Google, facebook and so on
 const provider = new GoogleAuthProvider();
 
-//you can have multiple providers; 
-//For let's say signInWithRedirect you can create a new provider than for signInWithPopup.  
+//you can have multiple providers;
+//For let's say signInWithRedirect you can create a new provider than for signInWithPopup.
 
 provider.setCustomParameters({
-  prompt: 'select_account' // force to select an account
+  prompt: "select_account", // force to select an account
 });
 
-//auth should always be ONE for the duration of use of this website. 
+//auth should always be ONE for the duration of use of this website.
 //You want the user to auth only once when using the website.
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider); 
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 // export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
-export const db = getFirestore(); 
+export const db = getFirestore();
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = collection(db, collectionKey);
   const batch = writeBatch(db);
 
   objectsToAdd.forEach((object) => {
-    const docRef = doc(collectionRef, object.title.toLowerCase())
+    const docRef = doc(collectionRef, object.title.toLowerCase());
     batch.set(docRef, object);
-  })
+  });
 
   await batch.commit();
-  console.log('done');
-}
+  console.log("done");
+};
 
 export const getCategoriesAndDocuments = async () => {
-  const collectionRef = collection(db, 'categories');
-  const q = query(collectionRef)
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
 
   //fetch the snapshot we want
-  const querySnapshot = await getDocs(q); 
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  },  {})
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
+};
 
-  return categoryMap;
-}
-
-export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
-  const userDocRef = doc(db, 'users', userAuth.uid); //connect to db and get user's uid
-  const userSnapshot = await getDoc(userDocRef); //get user information 
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  const userDocRef = doc(db, "users", userAuth.uid); //connect to db and get user's uid
+  const userSnapshot = await getDoc(userDocRef); //get user information
   //userSnapshot.exists() -> allows to see if the user exists
 
-
-  //if user doesn' exist => set the user with the userSnapshot 
-  if (!userSnapshot.exists()){
+  //if user doesn' exist => set the user with the userSnapshot
+  if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
-    const createdAt = new Date(); 
+    const createdAt = new Date();
 
-    try { 
+    try {
       await setDoc(userDocRef, {
         displayName,
-        email, 
+        email,
         createdAt,
-        ...additionalInformation
+        ...additionalInformation,
       });
     } catch (err) {
-      console.log("There was an error", err.message)
+      console.log("There was an error", err.message);
     }
   }
 
   //if user data  exists just return it;
   return userDocRef;
-}
+};
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-  if(!email || !password) return;
-  return await createUserWithEmailAndPassword(auth, email, password)
-}
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-  if(!email || !password) return;
-  return await signInWithEmailAndPassword(auth, email, password)
-}
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
+};
 
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => {
-  //open listener because it always listens for changes 
-  //you have to tell it to stop listening when the components unmounts > prevent memory leak 
-  onAuthStateChanged(auth, callback)
+  //open listener because it always listens for changes
+  //you have to tell it to stop listening when the components unmounts > prevent memory leak
+  onAuthStateChanged(auth, callback);
 };
