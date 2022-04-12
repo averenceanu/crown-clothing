c50a5e444e00d10f616244709ad4651fed937a46
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-04-11 23:12:25 -0400

    Changes to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,20 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import { rootReducer } from "./root.reducer";
 
-const logger = createLogger({});
-const middleWares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+  //here goes the code that we want our middleware to do
+  if (!action.type) {
+    return next(action);
+  }
+  console.log("type", action.type);
+  console.log("payload", action.payload);
+  console.log("state", store.getState());
+
+  next(action);
+  console.log("next state: ", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 //you need a root reduce to create a store
