3fe0f9524df16dc96223290ce0185c6b73733c47
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-04-20 14:12:07 -0400

    Add Typescript + convert reducer.utils to ts

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -3,3 +3,6 @@ import CATEGORIES_ACTION_TYPES from "./category.types";
 
 export const setCategories = (categoriesArray) =>
   createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+
+// export const fetchCategoriesFailed = (error) =>
+//   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
---
+++ src/store/categories/category.types.ts
@@ -0,0 +1,5 @@
+export enum CATEGORIES_ACTION_TYPES {
+  SET_CATEGORIES = "category/SET_CATEGORIES_MAP",
+}
+
+//enum allows to capture those strings as types themselves
