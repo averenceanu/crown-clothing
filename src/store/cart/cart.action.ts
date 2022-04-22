5bd959cf13f4150b544f4b2984a28a72bd306665
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-04-22 14:29:37 -0400

    Type cart store

---
+++ src/store/cart/cart.action.ts
@@ -0,0 +1,111 @@
+import { CategoryItem } from "../categories/category.types";
+import { CART_ACTION_TYPES, CartItem } from "./cart.types";
+import {
+  createAction,
+  withMatcher,
+  ActionWithPayload,
+  Action,
+} from "../../utils/reducer/reducer.utils";
+
+const removeCartItem = (
+  cartItems: CartItem[],
+  cartItemToRemove: CartItem
+): CartItem[] => {
+  //find the cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  //check it quantity is = to 1, if it is remove that item from the cart items
+  if (existingCartItem && existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  //return back cartitems with --
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? {
+          ...cartItem,
+          quantity: cartItem.quantity - 1,
+        }
+      : cartItem
+  );
+};
+
+const clearCartItem = (
+  cartItems: CartItem[],
+  cartItemToClear: CartItem
+): CartItem[] =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+//custom function that will allow to find the product in the array and increment the quantity
+const addCartItem = (
+  cartItems: CartItem[],
+  productToAdd: CategoryItem
+): CartItem[] => {
+  //find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+  //if found increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+          }
+        : cartItem
+    );
+  }
+  //return new array with modified cartItems/ new cart item
+  return [
+    ...cartItems,
+    {
+      ...productToAdd,
+      quantity: 1,
+    },
+  ];
+};
+
+export type SetIsCartOpen = ActionWithPayload<
+  CART_ACTION_TYPES.SET_IS_CART_OPEN,
+  boolean
+>;
+export type SetCartItems = ActionWithPayload<
+  CART_ACTION_TYPES.SET_CART_ITEMS,
+  CartItem[]
+>;
+
+export const setIsCartOpen = withMatcher(
+  (boolean: boolean): SetIsCartOpen =>
+    createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean)
+);
+
+export const setCartItems = withMatcher(
+  (cartItems: CartItem[]): SetCartItems =>
+    createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cartItems)
+);
+
+export const addItemToCart = (
+  cartItems: CartItem[],
+  productToAdd: CartItem
+): SetCartItems => {
+  const newCartItems = addCartItem(cartItems, productToAdd);
+  return setCartItems(newCartItems);
+};
+
+export const removeItemFromCart = (
+  cartItems: CartItem[],
+  cartItemToRemove: CartItem
+): SetCartItems => {
+  const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+  return setCartItems(newCartItems);
+};
+
+export const clearItemFromCart = (
+  cartItems: CartItem[],
+  cartItemToClear: CartItem
+): SetCartItems => {
+  const newCartItems = clearCartItem(cartItems, cartItemToClear);
+  return setCartItems(newCartItems);
+};
---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,48 @@
+import { AnyAction } from "redux";
+import { CartItem } from "./cart.types";
+import { setIsCartOpen, setCartItems } from "./cart.action";
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+};
+
+export const CAR_INITIAL_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+};
+
+export const cartReducer = (
+  state = CAR_INITIAL_STATE,
+  action: AnyAction
+): CartState => {
+  if (setIsCartOpen.match(action)) {
+    return {
+      ...state,
+      isCartOpen: action.payload!,
+    };
+  }
+  if (setCartItems.match(action)) {
+    return {
+      ...state,
+      cartItems: action.payload!,
+    };
+  }
+
+  return state;
+
+  // switch (type) {
+  //   case CART_ACTION_TYPES.SET_CART_ITEMS:
+  //     return {
+  //       ...state,
+  //       cartItems: payload,
+  //     };
+  //   case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+  //     return {
+  //       ...state,
+  //       isCartOpen: payload,
+  //     };
+  //   default:
+  //     return state;
+  // }
+};
